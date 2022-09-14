791bf3c08908e8d900f3e778d3f433d936bc8d3b
Sunnywillow <joseph3588.me@gmail.com>
2022-09-14 23:51:05 +0800

    2. Vue.js通过axios访问Flask RESTful API

---
+++ front-end/src/components/Ping.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Ping.vue",
+        data() {
+          return {
+            msg: 'Pong！'
+          }
+        },
+      methods: {
+          getMessage () {
+            const path = 'http://localhost:5000/api /pong'
+            axios.get(path)
+              .then((res) => {
+              this.msg = res.data;
+            })
+            .catch((error) => {
+              //  eslint-disable-next-line
+              console.error(error);
+            })
+
+
+          }
+      },
+      created() {
+          this.getMessage()
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
